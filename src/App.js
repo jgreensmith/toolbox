68e6d2201e2fe2109363562d3d4b40180ccc5ce8
James Greensmith <jgreensmith13@gmail.com>
2021-09-12 20:43:34 +0100

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <>toolbox</>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <>
+        <nav className="main-nav">
+            <div className="brand-container">
+                <Link to='/' className="home-link">
+                    <div className="brand-title" >Toolbox</div>
+                    <AppsIcon fontSize="large" className="icon" />
+                </Link>
+            </div>
+            
+            
+        </nav>
+        </>
+    )
+}
+
+export default Navbar;
